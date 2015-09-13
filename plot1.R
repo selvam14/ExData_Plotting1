3cb5d20a50d8424c1d36c35f956d44943eae5e9a
selvam14 <selvam.mohan14@gmail.com>
2015-09-13 14:29:58 +0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,10 @@
+setwd('C://Users//User//Documents//eda')
+dt=read.csv('household_power_consumption.txt',header=T, sep=';')
+dt$Datetime = paste(as.character(dt[,1]) , dt[,2])
+dt[,1]=as.Date(dt$Date,'%d/%m/%Y')
+dt = subset(dt, Date == '2007-02-01' | Date == '2007-02-02')
+dt[,3] = as.numeric(as.character(dt[,3])) 
+#Generate PNG graph
+png('plot1.png',width=480,height=480)
+hist(dt[,3],col='red',xlab='Global Active Power (kilowatts)', ylab='Frequency',main='Global Active Power')
+dev.off()
